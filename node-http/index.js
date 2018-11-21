f3342b019a96646f62802c2f92c888f16d403a92
Salim Huneidi <salimhuneidi@salims-macbook-pro.home>
2018-11-21 17:57:00 +0100

    Express Example

--- node-http/index.js
+++ node-http/index.js
@@ -1,14 +1,48 @@
 const http = require("http");
-
+const fs = require('fs');
+const path = require('path');
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req,resp) => {
-    console.log(req.headers);
+    console.log("Request for"+req.url+" by method" + req.method);
     
-    resp.statusCode = 200; 
-    resp.setHeader('content-Type','text/html');
-    resp.end('<html><body><h1>Hello, World!</h1></body></html>');
+    if (req.method =="GET"){
+        var fileUrl;
+        if(req.url == '/')  fileUrl= '/index.html';
+        else fileUrl = req.url; 
+
+        var filePath = path.resolve('./public'+fileUrl);
+        const fileExt = path.extname (filePath);
+        if (fileExt == '.html'){
+            fs.exists(filePath, (exists) => {
+                if(!exists){
+                    resp.statusCode = 404;
+                    resp.setHeader('Content-Type', 'text/html');
+                    resp.end('<html><body><h1>Error 404: ' + fileUrl + ' not found</h1></body></html>');
+
+                    return;
+                }
+                resp.statusCode = 200;
+                resp.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(resp);
+            })
+        }
+        else{
+            resp.statusCode = 404;
+            resp.setHeader('Content-Type', 'text/html');
+            resp.end('<html><body><h1>Error 404: ' + fileUrl + ' not an html</h1></body></html>');
+
+            return;
+        }
+    }
+    else{
+        esp.statusCode = 404;
+            resp.setHeader('Content-Type', 'text/html');
+            resp.end('<html><body><h1>Error 404: ' + req.method + ' not supported</h1></body></html>');
+
+            return;
+    }
 })
 
 server.listen(port, hostname, () => {
