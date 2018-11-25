76ace12ecc8c42d554af74d56c72bfbf4ea21c78
Salim Huneidi <salimhuneidi@salims-macbook-pro.home>
2018-11-25 17:22:41 +0100

    Expreds simple REST

--- node_express/index.js
+++ node_express/index.js
@@ -1,20 +1,65 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000; 
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req,resp,next) =>{
+    resp.statusCode = 200; 
+    resp.setHeader('content-type' , 'text-plain');
+    next();
+});
+
+app.get('/dishes', (req,resp,next) => {
+    resp.end('will send all the dishes to you!');
+});
+
+app.post('/dishes', (req,resp,next) => {
+    resp.end('will add the dish'+req.body.name+' with details' + req.body.description);
+});
+
+app.put('/dishes', (req,resp,next) => {
+    resp.statusCode = 403;
+    resp.end('PUT operation not supported on /dishes');
+});
+
+app.delete('/dishes' , (req, resp, next) => {
+    resp.end('Deleting all dishes!');
+});
+
+app.get('/dishes/:dishId', (req,resp,next) => {
+    resp.end('will send details of the dish: ' + req.params.dishId + ' to you');
+
+});
+
+app.post('/dishes/:dishId', (req,resp,next) => {
+    resp.end('POST operation not supported on /dishes/:id');
+});
+
+app.put('/dishes/:dishId', (req, resp, next) => {
+    resp.write('Updating the dish: ' + req.params.dishId + '\n');
+    resp.end('Will update the dish: ' + req.body.name + 
+          ' with details: ' + req.body.description);
+  });
+
+app.delete('/dishes/:dishId', (req,resp,next) => {
+    resp.end('Deleting dish: '+ req.params.dishId);
+});
+
 
 app.use(express.static(__dirname+'/public'));
 
-app.use((req,res,next)=> {
+app.use((req,resp,next)=> {
     
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/html');
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>')
+    resp.statusCode = 200;
+    resp.setHeader('Content-Type','text/html');
+    resp.end('<html><body><h1>This is an Express Server</h1></body></html>')
 
 
 });
